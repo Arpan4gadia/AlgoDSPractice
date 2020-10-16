0c92691039dbb8eae7194471655782a2ff251ef6
Arpan <arpan4gadia@gmail.com>
2020-10-16 17:52:27 +0530

    Assignment 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+//#include <stdio.h> 
+int main(int argc, char** argv) {
 
-int main(){
-    std::cout << "Hello World";
-    
     return 0;
+}
+
+/**
+ * @brief myPrimeScore: Write a functionn which calculates the score to an input number @param numberIn.
+ *                      Score is sum of all the prime numbers @param numberIn is divisible by.
+ *                      <Consider the prime numbers less than 20>
+ * \param
+ * 
+ * @param numberIn 
+ * @return int          Score.
+ */
+int PrimeScore(int numberIn){
+
+}
+
+
+/**
+ * @brief   Check if the \param year is a leap year i.e. year in which february month contains 29 days 
+ *          Leap year can be identified mathematically as year value which is divisible by 4 and, by 400 if divisible by 100 
+ * @param year 
+ * @return true     year is a leap year
+ * @return false    
+ */
+bool IsLeapYear(int year){
+
+}
+
+
+/**
+ * @brief Write a function greetMe Which takes you name and age as input.
+ *          -> It should greet saying "Hello <Name>"
+ *          -> You are xxxx weeks old
+ *          -> and xxxx decades old
+ */
+void greetMe(){
+
+}
+
+
+/**
+ * @brief prints the version of cpp used by compiler
+ * 
+ */
+void printCPPVersion(){
+    printf("__cplusplus = %d!\n", __cplusplus );
+
 }
