b6758eb7a97e8ca6a466c8ca9cc41c2237ae2234
SM <vishal.study@gmail.com>
2020-10-17 23:30:46 +0530

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 /**
  * @brief myPrimeScore: Write a functionn which calculates the score to an input number @param numberIn.
  *                      Score is sum of all the prime numbers @param numberIn is divisible by.
@@ -11,10 +10,43 @@ using namespace std;
  * @return int          Score.
  */
 int PrimeScore(int numberIn){
-
-}
-
-
+    int count = 0;
+    cout << numberIn << endl;
+    if (numberIn % 2 == 0) {
+        count += 2;
+        cout << count;
+    }
+    else if (numberIn % 3 == 0) {
+        count += 3;
+        cout << count;
+    }
+    else if (numberIn % 5 == 0) {
+        count += 5;
+        cout << count;
+    }
+    else if (numberIn % 7 == 0) {
+        count += 7;
+        cout << count;
+    }
+    else if (numberIn % 11 == 0) {
+        count += 11;
+        cout << count;
+    }
+    else if (numberIn % 13 == 0) {
+        count += 13;
+        cout << count;
+    }
+    else if (numberIn % 17 == 0) {
+        count += 17;
+        cout << count;
+    }
+    else if (numberIn % 19 == 0) {
+        count += 19;
+        cout << count;
+    }
+    cout << count;
+    return 0;
+    }
 /**
  * @brief   Check if the \param year is a leap year i.e. year in which february month contains 29 days 
  *          Leap year can be identified mathematically as year value which is divisible by 4 and, by 400 if divisible by 100 
@@ -23,10 +55,18 @@ int PrimeScore(int numberIn){
  * @return false    
  */
 bool IsLeapYear(int year){
-
+    int mod = year % 4;
+    int mod100 = year % 100;
+    int mod400 = year % 400;
+    if (mod == 0 || mod100 == 0 && mod400 == 0){
+        cout << "It is a leap year." << endl;
+        return true;
+    }
+    else {
+        cout << "It is not a leap year." << endl;
+        return false;
+    }
 }
-
-
 /**
  * @brief Write a function greetMe Which takes you name and age as input.
  *          -> It should greet saying "Hello <Name>"
@@ -34,21 +74,37 @@ bool IsLeapYear(int year){
  *          -> and xxxx decades old
  */
 void greetMe(){
-    cout << "Hi";
+    string name = "";
+    int age;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << endl;
+    cout << "Enter your age: ";
+    cin >> age;
+    float decades = age / 10;
+    float weeks = age * 52.14;
+    cout << "You are " << weeks << " weeks old." << endl;
+    cout << "You are " << decades << " decades old." << endl;
+
 }
-
-
 /**
  * @brief prints the version of cpp used by compiler
  * 
  */
 void printCPPVersion(){
-    printf("__cplusplus = %d!\n", __cplusplus );
+    printf("__cplusplus = %d!\n", __cplusplus);
 
 }
+int main() {
+    greetMe();
+    int myyear;
+    int number;
+    cout << "Enter a year: ";
+    cin >> myyear;
+    IsLeapYear(myyear);
+    cout << "Enter any number: ";
+    cin >> number;
+    PrimeScore(number);
+    printCPPVersion();
 
-
-int main(int argc, char** argv) {
-
-    return 0;
-}
+}   
